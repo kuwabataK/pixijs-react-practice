c1cca0708f82b49f42876dc497212a6584f49560
KuwabataK <>
2019-07-29 22:41:20 +0900

    Updates

---
+++ precache-manifest.0e1c7e92644b941cc1d8f1ecff8fc0ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf51c4e3c3deee1e9d1fa63ff1ebf29",
+    "url": "/pixijs-react-practice/index.html"
+  },
+  {
+    "revision": "6476a4766403faab4788",
+    "url": "/pixijs-react-practice/static/css/main.23579723.chunk.css"
+  },
+  {
+    "revision": "0cea23330c99b92b35ff",
+    "url": "/pixijs-react-practice/static/js/2.4eea6892.chunk.js"
+  },
+  {
+    "revision": "6476a4766403faab4788",
+    "url": "/pixijs-react-practice/static/js/main.3b4a7226.chunk.js"
+  },
+  {
+    "revision": "63599156740fce77080b",
+    "url": "/pixijs-react-practice/static/js/runtime~main.60fe0d45.js"
+  }
+]);
